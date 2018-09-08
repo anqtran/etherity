c2f09a7c86c0b59b31de4b2ac250658c2cb4fe6b
K <kiettran8795@gmail.com>
2018-09-08 18:25:06 -0400

    auctions.js

--- client/src/components/auction/Auction.js
+++ client/src/components/auction/Auction.js
@@ -6,42 +6,75 @@ import Countdown from './Countdown.js';
 import Spinner from '../common/Spinner';
 import { getAuction } from '../../actions/auctionActions';
 
+import TextFieldGroup from '../common/TextFieldGroup';
+
 import ImageGallery from 'react-image-gallery';
 
 class Auction extends Component {
   constructor(props) {
     super(props);
-    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      auction: {},
+      loading: '',
+      seller: {},
+      currentPrice: '',
+      errors: {}
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   componentDidMount() {
     this.props.getAuction(this.props.match.params.id);
+    const { auction, loading, error } = this.props.auction;
+    const seller = this.props.auction.seller;
+    this.setState({
+      auction,
+      loading,
+      seller,
+      errors: error
+    });
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.auction.profile === null && this.props.auction.loading) {
       this.props.history.push('/not-found');
     }
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
   }
 
-  handleChange(e) {
-    this.setState({ value: e.target.value });
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
   }
 
   render() {
-    const { auction, loading } = this.props.auction;
+    const { auction, errors, loading } = this.state;
 
+    const currentDate = new Date();
+    const year =
+      currentDate.getMonth() === 11 && currentDate.getDate() > 23
+        ? currentDate.getFullYear() + 1
+        : currentDate.getFullYear();
     let auctionContent;
     // console.log(auction);
     if (auction === null || loading || auction === '') {
       auctionContent = <Spinner />;
     } else {
-      console.log(auction);
+      // console.log(auction);
+
       auctionContent = (
         <div>
           <div className="row">
             <div className="col-md-6">
-              <Link to="/profiles" className="btn btn-light mb-3 float-left">
+              <Link to="/auctions" className="btn btn-light mb-3 float-left">
                 Back To Gallery
               </Link>
             </div>
@@ -62,31 +95,36 @@ class Auction extends Component {
           </div>
           <div className="row">
             <div className="col-lg-6 col-md-4 col-8">
-              <div>Here is the count down clock!!!!!!!!!!!!!!</div>
               <Countdown date={`${year}-12-24T00:00:00`} />
               {auction.bid ? (
                 <div>Highest Donator: {auction.bid.buyer} </div>
               ) : null}
             </div>
             <div className="col-lg-6 col-md-4 col-8">
-              <label>
-                {/* <h6 />
-                <TextFieldGroup
-                  placeholder="* Initial Price"
-                  name="basePrice"
-                  type="number"
-                  value={this.state.basePrice}
-                  onChange={this.onChange}
-                  error={errors.basePrice}
-                /> */}
-              </label>
+              <div className="row">
+                <h4>Highest Bid</h4>
+                {/* <h3>{auction.bid.highestPrice}</h3> */}
+              </div>
+              <div className="row">
+                <label>
+                  <h4>Go Bid!!!</h4>
+                  <TextFieldGroup
+                    placeholder="Bid Price"
+                    name="currentPrice"
+                    type="number"
+                    value={this.state.currentPrice}
+                    onChange={this.onChange}
+                    error={errors.currentPrice}
+                  />
+                </label>
+              </div>
 
-              {/* <Link
-                to={`/bid/${auction.handle}/${this.state.value}`}
+              <Link
+                to={`/bid/${auction.handle}/${this.state.currentPrice}`}
                 className="btn btn-secondary"
               >
                 Donate
-              </Link> */}
+              </Link>
             </div>
           </div>
         </div>
@@ -107,11 +145,13 @@ class Auction extends Component {
 
 Auction.propTypes = {
   getAuction: PropTypes.func.isRequired,
-  auction: PropTypes.object.isRequired
+  auction: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-  auction: state.auction
+  auction: state.auction,
+  errors: state.errors
 });
 
 export default connect(
