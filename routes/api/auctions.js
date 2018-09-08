605ed859352f81cceea406bc739b6d78bf3aa71a
K <kiettran8795@gmail.com>
2018-09-08 11:26:19 -0400

    update auctions.js

--- routes/api/auctions.js
+++ routes/api/auctions.js
@@ -18,7 +18,6 @@ router.get('/test', (req, res) => res.json({ msg: 'Auctions work' }));
 // @desc    Register user
 // @access  Public
 router.post('/create', (req, res) => {
-<<<<<<< HEAD
   const errors = {};
 
   const newAuction = new Auction({
@@ -38,58 +37,6 @@ router.post('/create', (req, res) => {
     .then(auction => res.json(auction))
     .catch(err => console.log(err));
 });
-=======
-
-	const errors = {};
-
-	const newAuction = new Auction({
-		seller: req.body.seller,
-		name: req.body.name,
-		images: req.body.images,
-		description: req.body.description,
-		short_description: req.body.description,
-		base_price: req.body.base_price,
-		bid: {
-			highestbid : req.body.base_price
-		}
-
-	});
-
-
-	newAuction
-            .save()
-            .then(auction => res.json(auction))
-            .catch(err => console.log(err));
-});
-
-// @route   GET api/auctions/:id
-// @desc    Get auction by id
-// @access  Public
-router.get('/:id', (req, res) => {
-  Auction.findById(req.params.id)
-    .then(auction => res.json(auction))
-    .catch(err =>
-      res.status(404).json({ noauctionfound: 'No auction found with that ID' })
-    );
-});
-
-
-// @route   DELETE api/auctions/:id
-// @desc    Delete auction
-// @access  Private
-router.delete(
-  '/:id',
-  (req, res) => {
-      Auction.findById(req.params.id)
-        .then(auction => {
-
-          // Delete
-          auction.remove().then(() => res.json({ success: true }));
-        })
-        .catch(err => res.status(404).json({ auction: 'No auction found' }));
-  }
-);
->>>>>>> aba999931723a3d91e92bdec33f7a7891ca5337a
 
 // @route   POST api/auctions/add
 // @desc    Add Auction
