8fd4ff04ce26c4ce29b41d416e83022c9d911786
An Q Tran <anqtran@gatech.edu>
2018-09-09 09:15:14 -0400

    changing footer credit

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default () => {
   return (
     <footer className="bg-dark text-white mt-5 p-4 text-center">
-      Copyright &copy; {new Date().getFullYear()} Crypto Chari by A&K
+      Copyright &copy; {new Date().getFullYear()} Crypto Chari by 209
     </footer>
   );
 };
